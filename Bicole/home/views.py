68541a27549149bbc8222d337d7b07af970e1ced
Saad Kardoudi <saad.kardoudi.24@gmail.com>
2021-02-09 00:22:40 +0100

    Add login and register front end

--- Bicole/home/views.py
+++ Bicole/home/views.py
@@ -4,3 +4,9 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request,'home/index.html')
+
+def login (request):
+    return render(request,'home/login.html')
+
+def register (request):
+    return render(request,'home/register.html')
